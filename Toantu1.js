fe16734a3ab9f0db7924ab8e93b119335c41d664
Your Name <you@example.com>
2019-11-04 10:12:22 +0700

    Dong

---
+++ Toantu1.js
@@ -0,0 +1,10 @@
+let inputWidth;
+let inputHeight;
+inputWidth=prompt ("Enter the width");
+inputHeight=prompt ("Enter the height");
+let width = parseFloat(inputWidth);
+let height = parseFloat (inputHeight);
+let area = width * height;
+document.write("The area is :" + area) ;
+
+
